74b4bdf3ed903f183864ebeac5c89df3160932ae
jmsB00nd <arabhamza4349@gmail.com>
2025-09-01 19:17:23 +0100

    added text ids generation

--- GPT2.py
+++ GPT2.py
@@ -74,7 +74,7 @@ class TransformerBlock(nn.Module):
 
 GPT_CONFIG_124M = {
     "vocab_size" : 50257,
-    "context_length" : 1024,
+    "context_length" : 256,
     "emb_dim" : 768,
     "n_heads" : 12,
     "n_layers" : 12,
